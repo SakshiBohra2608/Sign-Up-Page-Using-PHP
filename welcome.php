0473fb758254024a033509d3f5e09871a7108070
SakshiBohra2608 <105503295+SakshiBohra2608@users.noreply.github.com>
2023-08-07 16:45:35 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,33 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body style="align-content: center; background-color: aliceblue; ">
+
+    <div style="width: 600px; height:50% ; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;">
+        <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+    </div>
+    
+</body>
+</html>
